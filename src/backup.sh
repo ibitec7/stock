505a4319f20ffc72fc0bde391774e5be08700304
ibitec7 <syed.ibrahim.omer.2@gmail.com>
2025-03-18 00:36:30 +0800

    Remove deprecated backup and scrape scripts

--- src/backup.sh
+++
@@ -1,7 +0,0 @@
-firefox --search "$1" && sleep 1 \
-&& xdotool mousemove 200 500 && xdotool click 3 && xdotool mousemove 250 580 \
-&& xdotool click 1 && sleep .3 \
-&& xdotool type "$2" && xdotool key 0xff09 && xdotool key 0xff54 \
-&& xdotool key --repeat=2 0xff09 && xdotool key Return && xdotool key Tab && xdotool key Return\
-&& sleep .7 && xdotool key Return \
-&& sleep 0.2 && xdotool key --clearmodifiers Alt+F4
--- src/scrape.sh
+++
@@ -1,7 +0,0 @@
-firefox "$1" && sleep 1 \
-&& xdotool mousemove 158 749 && xdotool click 3 && xdotool mousemove 262 510 \
-&& xdotool click 1 && sleep .3 \
-&& xdotool type "~/stock/src/page1.html" && xdotool key 0xff09 && xdotool key 0xff54 \
-&& xdotool key --repeat=2 0xff09 && xdotool key Return && xdotool key Tab && xdotool key Return\
-&& sleep .7 && xdotool key Return \
-&& sleep 0.2 && xdotool key --clearmodifiers Alt+F4
