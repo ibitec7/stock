c99dbd1a99e15c1cd1742e0e064c40710aa8a5d3
ibitec7 <syed.ibrahim.omer.2@gmail.com>
2025-03-18 00:36:01 +0800

    Refactor news scraping functionality to use Selenium and async HTTP requests

--- src/news.py
+++ src/news.py
@@ -1,42 +1,135 @@
-# %%
 import yfinance as yf
-import polars as pl
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 import httpx
 import asyncio
 from bs4 import BeautifulSoup
-import time
+import os
+import logging
+import json
 
-# %%
-news = yf.Ticker("AAPL").news
-# %%
-news[0]["content"].keys()
-# %%
-async def get_response(client: httpx.AsyncClient, url: str) -> httpx.Response:
-    response = await client.get(url)
-    return response
-# So basically I am interested in the ['title', 'description', 'summary', 'pubDate', 'canonicalUrl']
-# %%
-relevant_news = []
-urls = []
-for story in news:
+OUTPUT_PATH = "/home/ibrahim/stock/data/historical.json"
+KEY_PATH = "/home/ibrahim/stock/keys.json"
 
-    # if any(sub in story["content"]["title"] for sub in ["nvidia", "NVDA", "Nvidia", "NVIDIA"]):
-        urls.append(story["content"]["canonicalUrl"]["url"])
-        relevant_news.append(story)
+async def get_response(client: httpx.AsyncClient, url: tuple):
+    response = await client.get(url[1])
+    response_tuple = (url[0], response)
 
-# %%
-async def main(urls: list):
+    if response.status_code == 200:
+        logging.debug(f"Fetched {url[1]} successfully!")
+    else:
+        logging.warning(f"Bad response from {url[1]}")
+
+    return response_tuple
+
+def get_response_sync(client: httpx.Client, url: tuple):
+    response = client.get(url[1])
+    response_tuple = (url[0], response)
+
+    if response.status_code == 200:
+        logging.debug(f"Fetched {url[1]} successfully!")
+    else:
+        logging.warning(f"Bad response from {url[1]}")
+
+    return response_tuple
+
+def scrape_selenium(urls: list):
+    options = Options()
+    options.add_argument("--headless")
+
+    logging.info("Created Selenium web driver")
+
+    driver = Chrome(options=options)
+
+    soups = []
+
+    logging.info("Fetching urls using selenium")
+
+    for i, url in urls:
+        driver.get(url)
+        html = driver.page_source
+        soups.append((i, BeautifulSoup(html, "html.parser")))
+
+    return soups
+
+async def fetch_urls(urls: list) -> list:
+
+    logging.info("Configuring async client")
+
     async with httpx.AsyncClient() as client:
         tasks = [asyncio.create_task(get_response(client, url)) for url in urls]
+
+        logging.info("Created tasks now gathering them")
         responses = await asyncio.gather(*tasks)
-    
+
     return responses
 
-if __name__ == "__main__":
-    responses = asyncio.run(main(urls))
-    for i,response in enumerate(responses):
-        print(response.url)
+def run_async(urls: list) -> list:
+    logging.info("Fetching URL responses through async client")
+    return asyncio.run(main(urls))
 
-    print(responses)
+def get_news(api_key: str, ticker: str, date_from: str, date_to, limit: int = 50) -> httpx.Response:
+    logging.info("Fetching news from Alpha Vantage API")
+    if date_from == None:
+        url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&limit={limit}&sort=RELEVANCE&apikey={api_key}"
+    else:
+        url = f"https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&time_from={date_from}&time_to={date_to}&limit={limit}&apikey={api_key}"
+
+    response = httpx.get(url)
+
+    if response.status_code == 200:
+        logging.info(f"Fetched {url[1]} successfully!")
+    else:
+        logging.warning(f"Bad response from {url[1]}")
+
+    return response
 
 
+### MAIN FUNCTION ###
+
+def main(ticker: str, from_date: str, to_date: str, limit: int, output_path: str, key_path: str, log_path: str):
+    
+    with open(key_path, "r") as file:
+        keys = json.load(file)
+
+    if os.path.exists(output_path) == False:
+        response = get_news(api_key=keys["ALPHA_VANTAGE_API_KEY"], ticker=ticker, date_from=from_date, limit=limit)
+        data = response.json()
+    
+    else:
+        logging.info("Reading from existing news file")
+        with open(output_path, "r") as file:
+            data = json.load(file)
+
+    urls = [(i,feed["url"]) for i, feed in enumerate(data["feed"])]
+
+    logging.info("fetching the news from the URLs")
+    responses = run_async(urls)
+
+    bad_urls = []
+
+    logging.info("Fetching detailed news from URLs")
+
+    for i, response in enumerate(responses):
+        if response[1].status_code == 200:
+            soup = BeautifulSoup(responses[i][1].content, "html.parser")
+            data["feed"][i]["response"] = soup.text
+        elif response[1].status_code == 429:
+            bad_urls.append((i, str(response.url)))
+
+    if len(bad_urls) == 0:
+        logging.info("No bad URLs found")
+    else:
+        logging.info("Some bad urls found scraping from selenium")
+
+        soups = scrape_selenium(bad_urls)
+
+        for i, soup in soups:
+            data["feed"][i]["response"] = soup.text
+
+    logging.info("Saving the json file now")
+
+    with open(output_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+    logging.info("Done saving the json file")
